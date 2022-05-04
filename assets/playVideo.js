9f19630cc6c18e6a9970be50b32fc8d83abc3c98
thanhdz <daybydaylove@gmail.com>
2022-05-04 15:35:17 +0700

    thanh: update code to branch thanh

---
+++ assets/playVideo.js
@@ -0,0 +1,13 @@
+const videoPlayer = $('.discover-vid');
+
+const videoBtn = $('.discover-play');
+
+const video = $('.control-video');
+
+
+
+videoPlayer.addEventListener('click', function(e)
+{
+  video.style.zIndex = '2';
+  // video.play();
+})
